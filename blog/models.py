61ffbfc46cb322d805485a4264a53731a027b589
Rodolfo <@Rodolfo@DESKTOP-6SKPNVL>
2021-04-01 17:26:00 -0300

    models.py

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
+
     STATUS = (
         ('rascunho', 'Rascunho'),
         ('publicado', 'Publicado'),
@@ -18,8 +20,9 @@ class Post(models.Model):
     status    = models.CharField(max_length=10,
                                  choices=STATUS,
                                  default='rascunho')
+
     class Meta:
-        ordering = ('publicado',)
+        ordering = ('-publicado',) #o - ordena do mais recente primeiro
 
     def __str__(self):
         return self.titulo
